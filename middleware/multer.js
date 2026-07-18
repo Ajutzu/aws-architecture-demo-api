a0e0010cdc94abd3219973ff9b452542fab357a1
AJ <aeronjamescastillo6@gmail.com>
2026-07-18 17:50:11 +0800

    fix: change upload export to default for multer configuration

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,10 +2,11 @@ import multer from "multer";
 
 // Configure multer to use memory storage and set a file size limit of 5MB for uploaded files.
 
-export const upload = multer({
+const upload = multer({
     storage: multer.memoryStorage(),
     limits: {
         fileSize: 5 * 1024 * 1024,
     },
 });
 
+export default upload;
